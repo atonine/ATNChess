cbf1bdd0937c36528427115937dc9d79c8089903
yuhongzhi <yuhongzhi@daojia.com>
2018-01-19 14:43:57 +0800

    初始化文件

---
+++ ATNChess/ATNChess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATNChess
+//
+//  Created by 于洪志 on 2018/1/19.
+//  Copyright © 2018年 于洪志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
